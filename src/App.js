26fa0a7a71fd91a92e71d3187d03f06e3921273e
Hiskia Pratama <hiskianggi@gmail.com>
2021-02-08 22:41:07 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "pages/LandingPage";
+
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
